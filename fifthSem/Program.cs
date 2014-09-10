854ff5adb85aec9d8b76a7231f2d6a00a23b9204
aasulv <aasulv.hilde@gmail.com>
2014-09-10 09:18:43 +0200

    test

--- fifthSem/Program.cs
+++ fifthSem/Program.cs
@@ -14,6 +14,7 @@ namespace fifthSem
         [STAThread]
         static void Main()
         {
+            //kommentar
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
