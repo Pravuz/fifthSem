b7c4d84fc43d335245d7e27ba8940561a6d99c94
Pravuz <situz.sj@gmail.com>
2014-09-16 14:27:30 +0200

    mer dataengine

--- fifthSem/DataEngine.cs
+++ fifthSem/DataEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,33 +10,35 @@ namespace fifthSem
 {
     class DataEngine
     {
-        public bool hasCom;
-        public static string hostname, filename;
+        public static string hostname;
         public int hostId, tempAlarmHigh, tempAlarmLow;
-        public const int REQUEST = 0, RECIEVE = 1, PUSH = 2;
+        public enum Type { REQUEST, RECIEVE, PUSH };
+        public enum Mode { MASTER, SLAVE };
 
-        private string logFolder = @"%USERPROFILE%\My Documents\Loggs\";
-        private string logFile;
-        private int sizeOfFile, timeSinceLastPush;
+        private string logFile, logFolder = @"%USERPROFILE%\My Documents\Loggs\";
+        private int sizeOfFile;
         private ScpHost mScpHost;
+        //private rs485host mrs485host;
+        //private alarmhost malarmhost;
 
-        /// <summary>
-        /// constructor
-        /// </summary>
         public DataEngine() {
             DateTime d = DateTime.Now;
-            logFile = d.Month.ToString() + d.Year.ToString();
+            logFile = d.Month.ToString() + d.Year.ToString() + ".txt";
 
             mScpHost = new ScpHost(1);
+            if (ComConnected()) mScpHost.CanBeMaster = true;
             mScpHost.ScpConnectionStatusEvent += ConnectionStatusHandler;
             mScpHost.PacketEvent += PacketHandler;
             hostname = ScpHost.Name;
 
             mScpHost.Start();
+            //start rs485 og alarmsystem
 
         }
 
         private void ConnectionStatusHandler(object sender, ScpConnectionStatusEventArgs e) {
+            string timeStamp = DateTime.Now.ToLongTimeString();
+
             switch (e.Status) { 
                 case ScpConnectionStatus.Master:
                     break;
@@ -58,12 +61,21 @@ namespace fifthSem
         /// <summary>
         /// Endrer program/klassevariabler basert på om bruker har huket av boks for tilkobling til rs485 (com)
         /// </summary>
-        private void isComConnected() { }
+        private bool ComConnected() {
+            return true;
+        }
         
         /// <summary> Method for writing logg/alarm/information etc to file </summary>
         /// <returns> True if the operation was a success and vice/versa. </returns>
-        private bool writeToFile(String[] s){
+        private bool writeToFile(string s){
             bool success = true;
+            try
+            {
+                File.AppendText(logFolder + logFile).Write(s);
+            }
+            catch (Exception e) {
+                success = false;
+            }
             return success;
         }
 
@@ -85,40 +97,6 @@ namespace fifthSem
         public temperatureReading(DateTime dateTime, double temp) {
             this.dateTime = dateTime;
             this.temp = temp;
-        }
-    }
-
-    /// <summary>
-    /// Creates an object of an alarm
-    /// </summary>
-    private class alarmEvent {
-
-        public const int ALARM_INFO = 0;
-        public const int ALARM_WARNING = 1;
-        public const int ALARM_ERROR = 2;
-
-        public int alarmId { get; set; }
-        public int alarmSenderId { get; set; }
-        public DateTime dateTime { get; set; }
-        public string alarmMsg { get; set; }
-        public int alarmSeverity { get; set; }
-
-        /// <summary>
-        /// An object representing an alarm-event.
-        /// </summary>
-        /// <param name="alarmSenderId">hostNameId</param>
-        /// <param name="dateTime">datetime value of the DateTime format</param>
-        /// <param name="alarmMsg">Alarmmessage</param>
-        /// <param name="alarmSeverity">An integer of the following value:
-        /// 0 = Information
-        /// 1 = Warning 
-        /// 2 = Error</param>
-        public alarmEvent(int alarmSenderId, DateTime dateTime, string alarmMsg, int alarmSeverity) {
-            alarmId = 0; //GET LAST ALARMID FROM LOG + 1
-            this.alarmMsg = alarmMsg;
-            this.alarmSenderId = alarmSenderId;
-            this.alarmSeverity = alarmSeverity;
-            this.dateTime = dateTime;
         }
     }
 
