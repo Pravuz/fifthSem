3beb37d3bcaab2b462d146e7174a4b0d7953fd77
Teo78 <anders.theodorsen@gmail.com>
2014-10-29 14:06:54 +0100

    kosmetikk

--- fifthSem/SCP/ScpPackets.cs
+++ fifthSem/SCP/ScpPackets.cs
@@ -22,7 +22,10 @@ namespace ScadaCommunicationProtocol
     }
     public abstract class ScpPacket
     {
-        protected enum ScpPacketTypes { RegRequest = 1, RegResponse = 51, LogFileRequest = 2, LogFileResponse = 52, AlarmRequest = 3, AlarmResponse = 53, MasterRequest = 4, MasterResponse = 54, TempBroadcast = 100, AlarmBroadcast = 101, AlarmLimitBroadcast = 102, AlarmFilterBroadcast = 103 };
+        protected enum ScpPacketTypes { RegRequest = 1, RegResponse = 51, LogFileRequest = 2, LogFileResponse = 52, 
+                                        AlarmRequest = 3, AlarmResponse = 53, MasterRequest = 4, MasterResponse = 54, 
+                                        TempBroadcast = 100, AlarmBroadcast = 101, AlarmLimitBroadcast = 102, 
+            AlarmFilterBroadcast = 103 };
         private static int newId = 0;
         public static int GetId()
         {
